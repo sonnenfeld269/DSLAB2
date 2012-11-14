2cb29011aecc30d69b31e4a3d60c2bea0745313b
delta458 <delta458@hotmail.com>
2012-11-14 23:20:50 +0100

    Inserted Log4J 2 and the xml configuration file.

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -10,27 +10,24 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-/**
- * This is just a temporary test Server class. For lookup of processEvents. Just
- * for RMI Demonstration. This can be for example the AuctionServer or
- * BillingServer.
- *
- * @author Dave
- */
 public class TestClass {
 
+    private static Logger logger = LogManager.getLogger(TestClass.class.getName());
+    
     private static final String HOST = "localhost";
     private static final int PORT = 1099;
     private static Registry registry;
 
     public static void main(String[] args) throws RemoteException, NotBoundException {
+        logger.debug("Test Class main started.");
         registry = LocateRegistry.getRegistry(HOST, PORT);
         AnalyticsServerInterface asi = (AnalyticsServerInterface) registry.lookup(AnalyticsServerInterface.class.getSimpleName());
-
-
+        
         Event e = new Event("TEST");
         asi.processEvents(e);
-        System.out.println("process event was called");
+        logger.fatal("process event was called");
     }
 }
