cbc0ee4c59efbeab4de583a6e9d0309d3dcac77b
delta458 <delta458@hotmail.com>
2012-11-20 21:40:26 +0100

    modfied ant file, created ManagementClientClasses

---
+++ src/ManagementClients/ManagementClient.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagementClients;
+
+/**
+ *
+ * @author Dave
+ */
+public class ManagementClient {
+    
+}
