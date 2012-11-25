0e2c7585f237d710f6a78dd42649976cf2297680
sanker <sanker@UbuntuHome>
2012-11-25 21:42:47 +0100

    -)Event package update

---
+++ src/Event/AnalyticsControllEvent.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Event;
+
+/**
+ *
+ * @author sanker
+ */
+public class AnalyticsControllEvent extends Event {
+    
+    private long Id;
+    
+    public AnalyticsControllEvent(long ID,AnalyticsControllEvent.AnalyticsControllEventType type)
+    {
+        super(type.name());
+        this.Id=ID;
+   
+    }
+    
+   public long getIDNumber()
+   {
+       return this.Id;
+   }
+   
+     public static enum AnalyticsControllEventType
+    {
+        CLOSE_FILTER,
+        ERROR_SUBSCRIPTION,
+        ERROR_UNSUBSCRIPTION
+        
+    }
+     
+    public String toString()
+    {
+        String msg = null;
+        if(super.getType().contains("CLOSE_FILTER"))
+        {
+            msg=""; 
+        }else  if(super.getType().contains("ERROR_SUBSCRIPTION"))
+        {
+             msg="Error: Subscription of ID  "+this.getIDNumber()+" was unsuccessfull."; 
+        }else  if(super.getType().contains("ERROR_UNSUBSCRIPTION"))
+        {
+             msg="Error: Unsubscription of ID  "+this.getIDNumber()+" was unsuccessfull."; 
+        }
+        return (msg);
+    }
+    
+}
